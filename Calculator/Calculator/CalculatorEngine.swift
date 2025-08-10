ff685728f2cc1d93632b72479246e85b3ff797a0
eceakcay <eceakcay.36@gmail.com>
2025-08-10 18:04:59 +0300

    storyboarrda ui yapıldı

---
+++ Calculator/Calculator/CalculatorEngine.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorEngine.swift
+//  Calculator
+//
+//  Created by Ece Akcay on 9.08.2025.
+//
+
+import Foundation
---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ece Akcay on 9.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func numberButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
